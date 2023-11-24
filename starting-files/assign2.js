14fe01c9ad34b8c97342decc0528fa61b99a4c1a
rapha <raphael_khan@outlook.com>
2023-11-24 02:27:11 -0700

    editing push

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -130,7 +130,7 @@ addEventListener("DOMContentLoaded", async (event) =>{
          });
          if(thisSearch!=null){
             let searchedValue = thisSearch.id;
-            if(searchedValue.value=''){
+            if(thisSearch.value=''){
                alert('Please choose an option.')
             }
             else{
