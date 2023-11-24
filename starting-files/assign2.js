6da6e318846e61a183b724c7d40e0a32fa8b2307
rapha <raphael_khan@outlook.com>
2023-11-23 20:36:37 -0700

    editing push

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -7,7 +7,6 @@ const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.p
 
 
 addEventListener("DOMContentLoaded", async (event) =>{
-   localStorage.clear();
 
    var playlist = localStorage.getItem('playlist');
 
