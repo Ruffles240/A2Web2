9e285ffe63373e6f9c48fb43da2e9365488826dc
rapha <raphael_khan@outlook.com>
2023-11-26 21:17:10 -0700

    editing push

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -299,7 +299,7 @@ addEventListener("DOMContentLoaded", async (event) =>{
          console.log('working');
          console.log(colHead.dataset.id);
          console.log(colHead.dataset.table);
-         colHead.classList.toggle("selected");
+         colHead.classList.add("selected");
          console.log(colHead.classList);
          colHead.firstChild.classList.remove('rotated');
 
