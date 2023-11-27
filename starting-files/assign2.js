ab45989428f0cfbd6ebbb33548d8b24783d2dbee
rapha <raphael_khan@outlook.com>
2023-11-27 02:24:42 -0700

    editing push

--- starting-files/assign2.js
+++ starting-files/assign2.js
@@ -225,9 +225,8 @@ addEventListener("DOMContentLoaded", async (event) =>{
       if(currentPlaylistSort!==null){
          playlist.sort(sortingFunctions[currentPlaylistSort]);
          let sortOrder = document.querySelector('.rearrange.playlistTableHead.selected');
-         if(typeof sortOrder !== 'undefined'){
-            if(sortOrder.firstChild.classList.contains('rotated')){
-            playlist.reverse();}
+         if(sortOrder != null && sortOrder.firstChild.classList.contains('rotated')){
+            playlist.reverse();
          }
       }
       populateTable(document.querySelector('#playlistTable'), playlist);
